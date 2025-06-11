5cd99d69da545c782b8a03ed89438a087c2408c1
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-06-11 16:19:29 +0200

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,24 @@
+# data.py
+aides_data = [
+    {
+        "nom": "RSA",
+        "conditions": lambda s, r, l, e: s == "Chômage" and r < 950,
+        "montant": 607
+    },
+    {
+        "nom": "APL",
+        "conditions": lambda s, r, l, e: l == "Oui" and r < 1200,
+        "montant": 180
+    },
+    {
+        "nom": "Prime d’activité",
+        "conditions": lambda s, r, l, e: s == "Salarié" and r < 1800,
+        "montant": 150
+    },
+    {
+        "nom": "Aide étudiante spécifique",
+        "conditions": lambda s, r, l, e: s == "Étudiant" and r < 600,
+        "montant": 100
+    },
+]
 
