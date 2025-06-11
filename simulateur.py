5115cc28071602850271cd8ff6e5ae4d3281d52e
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-06-11 16:11:17 +0200

    Update simulateur.py

--- simulateur.py
+++ simulateur.py
@@ -1,1 +1,11 @@
-
+def calculer_aides(situation, revenu, logement, enfants):
+    resultats = {}
+    if situation == "Chômage" and revenu < 950:
+        resultats["RSA"] = 607
+    if logement == "Oui" and revenu < 1200:
+        resultats["APL"] = 180
+    if situation == "Salarié" and revenu < 1800:
+        resultats["Prime d’activité"] = 150
+    if situation == "Étudiant" and revenu < 600:
+        resultats["Aide spécifique"] = 100
+    return resultats
