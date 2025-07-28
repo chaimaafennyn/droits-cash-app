44f9bd55c58776c697e2e562a1c66445bc8e289a
chaimaafennyn <chaimaafennyn8@gmail.com>
2025-07-28 10:28:20 +0200

    Delete simulateur.py

--- simulateur.py
+++
@@ -1,9 +0,0 @@
-# simulateur.py
-from data import aides_data
-
-def calculer_aides(situation, revenu, logement, enfants):
-    resultats = {}
-    for aide in aides_data:
-        if aide["conditions"](situation, revenu, logement, enfants):
-            resultats[aide["nom"]] = aide["montant"]
-    return resultats
